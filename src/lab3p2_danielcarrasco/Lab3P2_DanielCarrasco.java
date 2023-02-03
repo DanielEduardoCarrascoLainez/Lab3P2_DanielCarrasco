a444384dcdf1a734da92382eaee05f1fc76857d4
DanielEduardoCarrascoLainez <dcarrasco04@unitec.edu>
2023-02-03 13:05:52 -0600

    Inicio

---
+++ src/lab3p2_danielcarrasco/Lab3P2_DanielCarrasco.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab3p2_danielcarrasco;
+
+
+public class Lab3P2_DanielCarrasco {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
